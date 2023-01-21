c6a2627af5e975a45be268e26b63b0eb854e5cb7
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-01-21 13:33:26 -0300

    Feat: route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import Home from "./pages/home/home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path='/' element={ <Home/>}/>
+          <Route path='/projetos' element={ <Home/>}/>
+          <Route path='/sobre' element={ <Home/>}/>
+          <Route path='/estudos' element={ <Home/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
