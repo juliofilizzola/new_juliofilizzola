566fe8d03301a9a4cd927fa81b424935b0e7f669
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-01-21 13:34:03 -0300

    feat: add menu from project

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,20 +1,20 @@
-import presentation from '../../jsons/presentation.json';
-import './css/index.css';
+import presentation from "../../jsons/presentation.json";
+import Menu from "../menu/menu";
+import "./css/index.css";
 
 function Home() {
-  return(
-          <div className="home">
-         <h1>{presentation.title}</h1>
-          <div>
-            <h2>{presentation.title}</h2>
-            <div>
-              <p>
-                {presentation.text}
-              </p>
-            </div>
-          </div>
-          </div>
-          );
+  return (
+    <div className="home">
+      <Menu/>
+      <h1>{presentation.title}</h1>
+      <div>
+        <h2>{presentation.title}</h2>
+        <div>
+          <p>{presentation.text}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
